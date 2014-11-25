2252fd6fbe650c072b5ef9f8f09d61e2ad1e07c0
pakkumannn <p.antoine.cormerais@gmail.com>
2014-11-25 16:47:56 +0000

    création du menu en en-tete

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <link href="./css/StyleIndex.css" rel="stylesheet" media="all" type="text/css">
+</head>
+
+<body>
+  <!-- en-tête de la page -->
+  <div id="haut">
+    <!-- Affichge du menu par defaut titre de la page. Celui-ci disparait lors du passage de la souris-->
+    <div id="menu0"> 
+      TITRE APPLI 
+    </div>
+    <!--Affichage du menu de recherche. Celui-ci s'affiche lors du passage de la souris -->
+    <div id="menu1">
+      
+    </div>
+    
+</div>
+  <div id=corp>
+  </div>
+
+</body>
+
+</html>
